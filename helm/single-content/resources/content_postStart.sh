ccf957eb29a79d69aa0f25b4c4295fc66509e7ed
daniel-lau-mf <36928012+daniel-lau-mf@users.noreply.github.com>
2024-01-24 12:45:49 +0000

    Fix content_postStart for IDOL_SSL

--- helm/single-content/resources/content_postStart.sh
+++ helm/single-content/resources/content_postStart.sh
@@ -2,21 +2,12 @@
 # (c) Copyright 2023 Micro Focus or one of its affiliates.
 # END COPYRIGHT NOTICE
 
+source /content/startup_utils.sh
+
 function getHostname() {
   host=$(cat /etc/hostname)
   domain=$(cat /etc/resolv.conf | grep search | awk '{print $2}')
   hostname=${host}.{{ .Values.name }}.${domain}
-}
-
-function waitForAci() {
-  exit_code=1
-  while [ $exit_code -ne 0 ]; do
-    curl -o - "http://$1:$2/a=getpid" | grep "<autn:pid>"
-    exit_code=$?
-    if [ $exit_code -ne 0 ]; then
-      sleep 1
-    fi
-  done
 }
 
 logfile=/opt/idol/content/index/poststart.log
@@ -26,4 +17,4 @@ IDOL_CONTENT_ACI_PORT=${IDOL_CONTENT_SERVICE_PORT_ACI_PORT:-{{ .Values.aciPort |
 echo "[$(date)] hostname: ${hostname}" | tee -a $logfile
 
 # Final check that this Content is available
-waitForAci localhost ${IDOL_CONTENT_ACI_PORT}
+waitForAci localhost:${IDOL_CONTENT_ACI_PORT}
