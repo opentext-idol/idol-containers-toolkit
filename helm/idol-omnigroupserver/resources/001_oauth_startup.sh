a9ffc6bbe0367d7c73226dcd1061cfd414456b37
James Poole <james.poole2@microfocus.com>
2024-01-24 12:15:51 +0000

    Rename sourcedstartup file for easier mounting

--- helm/idol-omnigroupserver/resources/001_oauth_startup.sh
+++ helm/idol-omnigroupserver/resources/001_oauth_startup.sh
@@ -1,5 +1,11 @@
 #/bin/bash
 
-source ./utils/oauth_tool_setup.sh
+source ./oauth_tool_setup_functions
 
-handle_oauth_setup ${IDOL_COMPONENT} ${IDOL_COMPONENT_CFG} ${OAUTH_TOOL_CFG}
+if [[ ! -e ${OAUTH_TOOL_CFG} ]]
+then
+    echo No oauth_tool.cfg provided at ${OAUTH_TOOL_CFG}: OAuth will not be automatically configured via oauth_tool
+else
+    echo Automatically configuring OAuth using oauth_tool and ${OAUTH_TOOL_CFG}
+    handle_oauth_setup ${IDOL_COMPONENT} ${IDOL_COMPONENT_CFG} ${OAUTH_TOOL_CFG}
+fi
