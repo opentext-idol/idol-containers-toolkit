1be94cafeb92b5b4dadb3d5727d24c42d9203624
James Poole <james.poole2@microfocus.com>
2024-01-25 15:22:45 +0000

    Add/update copyright notices

--- helm/idol-omnigroupserver/resources/001_oauth_startup.sh
+++ helm/idol-omnigroupserver/resources/001_oauth_startup.sh
@@ -1,5 +1,16 @@
 #/bin/bash
 
+# BEGIN COPYRIGHT NOTICE
+# Copyright 2023-2024 Open Text.
+# 
+# The only warranties for products and services of Open Text and its affiliates and licensors
+# ("Open Text") are as may be set forth in the express warranty statements accompanying such
+# products and services. Nothing herein should be construed as constituting an additional warranty.
+# Open Text shall not be liable for technical or editorial errors or omissions contained herein.
+# The information contained herein is subject to change without notice.
+#
+# END COPYRIGHT NOTICE
+
 source /${IDOL_COMPONENT}/prestart_scripts/oauth_tool_setup_functions
 
 if [[ ! -e ${OAUTH_TOOL_CFG} ]]
