f4a5d22a7f93c82a5eb501c0068465b2f94a11bd
daniel-lau-mf <36928012+daniel-lau-mf@users.noreply.github.com>
2024-09-19 17:07:16 +0100

    Add basic tests for more charts
    Signed-off-by: daniel-lau-mf <36928012+daniel-lau-mf@users.noreply.github.com>

--- helm/test/test.py
+++ helm/test/test.py
@@ -11,6 +11,25 @@ class TestIdolAnswerServer(AciTestBase, unittest.TestCase):
     chartpath = os.path.join('..','idol-answerserver')
     _kinds = ['StatefulSet','Ingress','ConfigMap','Service']
 
+class TestIdolCommunity(AciTestBase, unittest.TestCase):
+    chartpath = os.path.join('..','idol-community')
+
+class TestIdolEductionServer(AciTestBase, unittest.TestCase):
+    chartpath = os.path.join('..','idol-eductionserver')
+
+class TestIdolMediaServer(AciTestBase, unittest.TestCase):
+    chartpath = os.path.join('..','idol-mediaserver')
+
+class TestIdolOmniGroupServer(AciTestBase, unittest.TestCase):
+    chartpath = os.path.join('..','idol-omnigroupserver')
+    _kinds = ['StatefulSet','Ingress','ConfigMap','Service']
+
+class TestIdolQms(AciTestBase, unittest.TestCase):
+    chartpath = os.path.join('..','idol-qms')
+
+class TestIdolView(AciTestBase, unittest.TestCase):
+    chartpath = os.path.join('..','idol-view')
+
 class TestSingleContent(AciTestBase, unittest.TestCase):
     chartpath = os.path.join('..','single-content')
     _kinds = ['StatefulSet','Ingress','ConfigMap','Service']
