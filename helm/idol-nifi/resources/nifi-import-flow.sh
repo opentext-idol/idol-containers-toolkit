71ca19e077d50b90a26ad8ff52be06c430eb70a6
sam-jeffrey-mf <sjeffrey2@opentext.com>
2024-10-25 10:35:48 +0100

    helm/idol-nifi: optional postgresql state db
    Uses the bitnami postgresql-ha chart to allow the option of db replicas if desired (by default Values.yaml creates a single instance)

--- helm/idol-nifi/resources/nifi-import-flow.sh
+++ helm/idol-nifi/resources/nifi-import-flow.sh
@@ -150,7 +150,12 @@ do
             ${NIFITOOLKITCMD} nifi set-param -pcid "${PARAMCONTEXT}" -pn "LicenseServerHost" -pv "{{ (index .Values "idol-licenseserver").licenseServerService }}"
             ${NIFITOOLKITCMD} nifi set-param -pcid "${PARAMCONTEXT}" -pn "LicenseServerACIPort" -pv "{{ (index .Values "idol-licenseserver").licenseServerPort }}"
             ${NIFITOOLKITCMD} nifi set-param -pcid "${PARAMCONTEXT}" -pn "IndexHost" -pv "{{ .Values.indexserviceName }}"
-            ${NIFITOOLKITCMD} nifi set-param -pcid "${PARAMCONTEXT}" -pn "IndexACIPort" -pv "{{ .Values.indexserviceACIPort }}"
+{{- if (index .Values "postgresql-ha" ).enabled }}
+            ${NIFITOOLKITCMD} nifi set-param -pcid "${PARAMCONTEXT}" -pn "PostgreSQLServer" -pv "{{ .Release.Name }}-postgresql-ha-pgpool"
+            ${NIFITOOLKITCMD} nifi set-param -pcid "${PARAMCONTEXT}" -pn "PostgreSQLDatabase" -pv "{{ (index .Values "postgresql-ha").postgresql.database }}"
+            ${NIFITOOLKITCMD} nifi set-param -pcid "${PARAMCONTEXT}" -pn "PostgreSQLUser" -pv "{{ (index .Values "postgresql-ha").postgresql.username }}" -ps true
+            ${NIFITOOLKITCMD} nifi set-param -pcid "${PARAMCONTEXT}" -pn "PostgreSQLPassword" -pv "{{ (index .Values "postgresql-ha").postgresql.password }}" -ps true
+{{- end}}
         fi
 
         echo "[$(date)] FLOWFILE ${FLOWFILE} imported to ProcessGroup: ${PROCESSGROUP}."
