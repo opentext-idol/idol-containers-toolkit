e6298fa285dce50e875855de1666c7a1da4f2522
daniel-lau-mf <36928012+daniel-lau-mf@users.noreply.github.com>
2025-04-07 14:11:39 +0100

    Extend test_replicas to all charts
    Signed-off-by: daniel-lau-mf <36928012+daniel-lau-mf@users.noreply.github.com>

--- helm/test/test.py
+++ helm/test/test.py
@@ -11,14 +11,6 @@ HelmChartTestBase.debug = False
 class TestIdolLibraryExample(AciTestBase, StatefulSetTests, unittest.TestCase):
     chartpath = os.path.join('..','idol-library-example-test')
     _kinds = ['Deployment','StatefulSet','Ingress','ConfigMap','Service']
-    def test_replicas(self):
-        ''' replicas can be set '''
-        replicas = 0
-        objs = self.render_chart({'name': 'test', 'replicas': replicas})
-        for k in ['Deployment', 'StatefulSet']:
-            ss = objs.get(k, {}).get('test', None)
-            self.assertIsNotNone(ss)
-            self.assertEqual(ss['spec']['replicas'], replicas)
 
 class TestIdolAnswerServer(AciTestBase, unittest.TestCase):
     chartpath = os.path.join('..','idol-answerserver')
@@ -47,6 +39,7 @@ class TestIdolView(AciTestBase, unittest.TestCase):
 class TestSingleContent(AciTestBase, StatefulSetTests, unittest.TestCase):
     chartpath = os.path.join('..','single-content')
     _kinds = ['StatefulSet','Ingress','ConfigMap','Service']
+
 
 if __name__ == '__main__':
     unittest.main()
